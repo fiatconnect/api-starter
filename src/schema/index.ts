f6b0a3d4e149cca350412da36a8931175b1893c5
Charlie Andrews <charlie.andrews@valoraapp.com>
2022-11-15 13:25:09 -0800

    refactor(zod): remove ajv

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,27 +1,7 @@
-import Ajv from 'ajv'
 import { ValidationError } from '../types'
 
 import { z, ZodError } from 'zod'
 import { ZodType } from 'zod/lib/types'
-
-const ajv = new Ajv({})
-
-/**
- * Validate AJV schema
- *
- * @deprecated - prefer validateZodSchema (more re-usable with fiatconnect-types)
- *
- * @param data
- * @param schema
- */
-export function validateSchema<T>(data: any, schema: string): T {
-  if (ajv.validate(schema, data)) return data
-
-  throw new ValidationError(
-    `Error while validating schema: invalid ${schema}`,
-    ajv.errors,
-  )
-}
 
 /**
  * Validate zod schema
